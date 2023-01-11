5e49265c4d8d453d5c4cb466a7e5386348d9dfc7
Ryan Christian <rchristian@ryanchristian.dev>
2023-01-10 19:01:16 -0600

    refactor: Update site to reflect current templates

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -1,31 +1,24 @@
-import {h} from 'preact';
-import {useState, useEffect} from 'preact/hooks';
-import style from './style';
+import { useEffect, useState } from 'preact/hooks';
 
 // Note: `user` comes from the URL, courtesy of our router
-export default ({ user }) => {
+export default function Profile({ user }) {
 	const [time, setTime] = useState(Date.now());
 	const [count, setCount] = useState(10);
+
 	useEffect(() => {
-		const timer = setInterval(() => {
-			setTime(Date.now());
-		}, 1000);
-		return () => {
-			clearInterval(timer);
-		}
+		let timer = setInterval(() => setTime(Date.now()), 1000);
+		return () => clearInterval(timer);
 	}, []);
+
 	return (
-		<div class={style.profile}>
+		<div>
 			<h1>Profile: {user}</h1>
-			<p>This is the user profile for a user named { user }.</p>
+			<p>This is the user profile for a user named {user}.</p>
 
 			<div>Current time: {new Date(time).toLocaleString()}</div>
 
 			<p>
-				<button onClick={() => {
-					setCount(count + 1);
-				}}>Click Me</button>
-				{' '}
+				<button onClick={() => setCount(count => count + 1)}>Click Me</button>{' '}
 				Clicked {count} times.
 			</p>
 		</div>
